9a760dd732cfdd44b60204a75ef1613e333b3336
mrcampa404 <carloscampaneros@gmail.com>
2021-04-17 13:26:05 -0600

    prueba dos

---
+++ Back/Usuario.py
@@ -0,0 +1,8 @@
+class Usuario(): 
+
+    def __init__(self,id,contrasenia,nombre,apellido,edad):  
+        self.id = id
+        self.password = contrasenia
+        self.nombre = nombre
+        self.apellido = apellido 
+        self.edad = edad 
---
+++ Back/main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import jsonify
+from Usuario import Usuario
+from flask_cors import CORS, cross_origin
+from flask import request
+
+def create_app(): 
+    app = Flask(__name__)
+    CORS(app) 
+    return app 
+
+app = create_app()
+usuarios = []  
+
+#creacion del contenido o url's 
+@app.route('/api/users',methods=['GET'])
+def get_users(): 
+    response = []
+    for i in usuarios: 
+        response.append({'nombre':i.id,'contrasenia':i.password})
+    return jsonify(response) 
+
+@app.route('/api/users/<id>',methods=['GET'])
+def get_user(id): 
+    for i in usuarios: 
+        if i.nombre == id: 
+            return jsonify(i.contrasenia)
+    return jsonify({'response':'no existe el usuario'})
+
+
+@app.route('/api/insertuser',methods=['POST'])
+def create_user(): 
+    json = request.get_json(force=True) 
+    if not (json.get('id') is None) and not (json.get('password') is None) and not (json.get('nombre') is None) and not (json.get('apellido') is None) and not (json.get('edad') is None): 
+        usuarios.append(Usuario(json.get('id'),json.get('password'),json.get('nombre'),json.get('apellido'),json.get('edad')))
+        return jsonify({'response':'ok'}) 
+    else: 
+        return jsonify({'response':'no ok'}) 
+
+    
+
+if __name__ == '__main__': 
+    app.run(debug=True) 
